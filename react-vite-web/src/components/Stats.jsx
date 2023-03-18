0a4587fb310433021a376819c6af15065ddef630
MSI\haoyi <haoyin_chong@hotmail.com>
2023-03-19 00:43:35 +0800

    completed business and billing section

--- react-vite-web/src/components/Stats.jsx
+++ react-vite-web/src/components/Stats.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { stats } from "../constants";
+import styles from "../style";
 
 const Stats = () => {
-  return <div>Stats</div>;
+  return (
+    <section
+      className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}
+    >
+      {stats.map((stat) => {
+        return (
+          <div
+            key={stat.id}
+            className={`${styles.flexCenter} flex-1 flex-row m-3`}
+          >
+            <h4
+              className={`font-poppins font-semibold xs:text-[40px] text-[30px] xs:leading-[53px] leading-[43px] text-white`}
+            >
+              {stat.value}
+            </h4>
+            <p
+              className={`uppercase ml-3 text-gradient font-poppins font-normal xs:text-[20px] text-[15px] xs:leading-[26px] leading-[21px]`}
+            >
+              {stat.title}
+            </p>
+          </div>
+        );
+      })}
+    </section>
+  );
 };
 
 export default Stats;
